fa34a4e72dda3dfb3df1ec8b8fcb8c250bbfbf80
David <davidjianghd@yahoo.ca>
2024-11-07 12:24:08 -0500

    discovered incomplete data for 2021, script to fix

--- filtering.py
+++ filtering.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 df = pd.read_csv("pull_requests_with_checkbox_data.csv")
 df = df[df['Type of Change'].isin(['New feature (which adds functionality to an existing integration)', 'New integration (thank you!)'])]
+df = df.drop_duplicates()
 df = df.reset_index(drop=True)
 print(df.head())
 df.to_csv("filtered_prs.csv", index=False)
