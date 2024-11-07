01b9389b3f351a024715f2ef7006d630a0d3bd91
David <davidjianghd@yahoo.ca>
2024-11-06 23:50:17 -0500

    filtered dataset

---
+++ filtering.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+
+df = pd.read_csv("pull_requests_with_checkbox_data.csv")
+df = df[df['Type of Change'].isin(['New feature (which adds functionality to an existing integration)', 'New integration (thank you!)'])]
+df = df.reset_index(drop=True)
+print(df.head())
+df.to_csv("filtered_prs.csv", index=False)
